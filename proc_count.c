9b7892b446a50a6c5de7a5e41e9c7f715b6c7703
Nick Bailey <nickbailey@ucla.edu>
2024-04-08 21:47:19 -0700

    get proc_count to output a line of text to the virtual file for each process

--- proc_count.c
+++ proc_count.c
@@ -8,6 +8,12 @@ static struct proc_dir_entry *entry;
 
 static int proc_count(struct seq_file *m, void *v){
 	// TODO: it's all yours
+  int count = 0;
+  struct task_struct *p;
+  for_each_process(p) {
+    seq_printf(m, "count\n");
+  }
+  
 	return 0;
 }
 
@@ -27,6 +33,6 @@ static void __exit proc_count_exit(void)
 module_init(proc_count_init);
 module_exit(proc_count_exit);
 
-MODULE_AUTHOR("Your Name");
+MODULE_AUTHOR("Nick Bailey");
 MODULE_DESCRIPTION("CS111 lab0 count proc number");
 MODULE_LICENSE("GPL");
